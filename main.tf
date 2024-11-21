b15b9183630052555b2184aaa35c1ea5062072ba
kumoora <132431831+kumoora@users.noreply.github.com>
2024-11-21 13:47:50 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ terraform {
 provider "snowflake" {
 }
 
-resource "snowflake_database" "demo_db" {
-  name    = "DEMO_DB"
+resource "snowflake_database" "demo_db_1" {
+  name    = "DEMO_DB_1"
   comment = "Database for Snowflake Terraform demo"
 }
