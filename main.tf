635917c4338551357c1de1a84d3c9b232f1d2e4b
kumoora <kumar.s.singh@kipi.bi>
2024-11-21 13:58:37 +0530

    adding schema in DB

--- main.tf
+++ main.tf
@@ -22,3 +22,9 @@ resource "snowflake_database" "demo_db_1_1" {
   name    = "DEMO_DB_1_1"
   comment = "Database for Snowflake Terraform demo"
 }
+
+resource "snowflake_schema" "demo_schema" {
+  database = snowflake_database.demo_db_1_1.name
+  name     = "DEMO_SCHEMA"
+  comment  = "Schema for Snowflake Terraform demo"
+}
